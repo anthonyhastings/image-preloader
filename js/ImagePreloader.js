fc83439ec09e581415eef5db4087fad2933f8cb9
Anthony Hastings <anthony@rehabstudio.com>
2014-10-10 17:05:46 +0100

    - Adding CommonJS support.

--- js/ImagePreloader.js
+++ js/ImagePreloader.js
@@ -10,6 +10,8 @@
 
     if (typeof(define) === 'function' && define.amd) {
         define(factory);
+    } else if (typeof(exports) === 'object') {
+        module.exports = factory();
     } else {
         root.ImagePreloader = factory();
     }
